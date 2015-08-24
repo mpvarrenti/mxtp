6068128b86be706ca7db8f2184fdec494663f402
Jenny Tran <tran.j@live.com>
2015-08-24 11:51:10 +1000

    index and show pages functioning

--- tapedeck/app/controllers/playlist_controller.rb
+++ tapedeck/app/controllers/playlist_controller.rb
@@ -1,6 +1,5 @@
 class PlaylistController < ApplicationController
   def index
-
   end
 
   def new
@@ -15,6 +14,6 @@ class PlaylistController < ApplicationController
 
   def show
     @playlist = Playlist.find params[:id]
-    @songs = PlaylistSongs.where(:id => @playlist.id)
+    @songs = PlaylistSong.where(playlist_id: @playlist)
   end
 end
