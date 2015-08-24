fb08241ca4f26a0979f5a3aa2f0cdbed667fe4e5
Jenny Tran <tran.j@live.com>
2015-08-24 10:55:13 +1000

    first bash

--- tapedeck/app/controllers/playlist_controller.rb
+++ tapedeck/app/controllers/playlist_controller.rb
@@ -1,10 +1,13 @@
 class PlaylistController < ApplicationController
   def index
+
   end
 
   def create
   end
 
   def show
+    @playlist = Playlist.find params[:id]
+    @songs = PlaylistSongs.where(:id => @playlist.id)
   end
 end
