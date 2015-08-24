891960b09a0d5aa2809017d6a75f12e77ce023ae
Michael Varrenti <mpvarrenti@gmail.com>
2015-08-24 20:40:32 +1000

    begun create playlist logic

--- tapedeck/app/controllers/playlist_controller.rb
+++ tapedeck/app/controllers/playlist_controller.rb
@@ -2,6 +2,13 @@ class PlaylistController < ApplicationController
   def index
   end
 
+  def new
+	playlist_new = Playlist.find(1)
+	playlist_new_songs = PlaylistSong.where(playlist_id:1)
+	
+	
+  end
+
   def create
   end
 
