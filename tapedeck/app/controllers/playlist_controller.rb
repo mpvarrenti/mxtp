6f4fc07212d0c7d2f6ac67a14356a16e7b2d0d2d
Michael Varrenti <mpvarrenti@gmail.com>
2015-08-26 23:01:59 +1000

    pre-lunch refactoring

--- tapedeck/app/controllers/playlist_controller.rb
+++ tapedeck/app/controllers/playlist_controller.rb
@@ -1,72 +1,85 @@
 class PlaylistController < ApplicationController
-  def index
-  end
+                
+        def index
+        end
 
-  def new
-    playlist_new = Playlist.create
-    redirect_to edit_playlist_path(playlist_new)
-  end
+        def new
+		# create a new blank playlist
+                playlist = Playlist.create
+                redirect_to edit_playlist_path(playlist)
+        end
 
-  def edit
+        def edit
+		# get relevant playlist
+                @playlist = Playlist.find(params[:id])
 
-          @playlist = Playlist.find(params[:id])
+		# update style record if such paramaters exist
+                if params[:style]
+			# create a new playlist_style record where none exists
+                        if PlaylistStyle.where(playlist_id:@playlist.id).count == 0
+                                playlist_style = PlaylistStyle.new
+                                playlist_style.playlist_id = @playlist.id
+			# otherwise get existing record
+			else
+				playlist_style = PlaylistStyle.where(playlist_id:@playlist.id)
+			end
 
-          if params[:style]
-                  if PlaylistStyle.where(playlist_id:@playlist.id).count == 0
-                          playlist_style_new = PlaylistStyle.new
-                          playlist_style_new.playlist_id = @playlist.id
-                          playlist_style_new.background_color = params[:style][:bgcolor]
-                          playlist_style_new.color = params[:style][:color]
-                          playlist_style_new.save
-                  end
-          end
-          
-          if params[:playlist]
-            @playlist.title = params[:playlist][:title]
-            @playlist.save
-          end
+			# set playlist_style properties and save
+			playlist_style.background_color = params[:style][:bgcolor]
+			playlist_style.color = params[:style][:color]
+			playlist_style.save
+                end
 
-          @playlist.title == nil ? @title = "Untitled" : @title = @playlist.title
+		# set title
+                if params[:playlist]
+                        @playlist.title = params[:playlist][:title]
+                        @playlist.save
+                end
 
-          if params[:new_song]
-            song_new = PlaylistSong.new
-            song_new.playlist_id = @playlist.id
-            song_new.song_title = params[:new_song][:song_title]
-            song_new.artist_name = params[:new_song][:artist_name]
-            song_new.save
-          end
+                @playlist.title == nil ? @title = "Untitled" : @title = @playlist.title
 
-          @songs = PlaylistSong.where(playlist_id:@playlist.id)
-  end
+		# add new song to playlist
+                if params[:new_song]
+			# new blank song
+                        song= PlaylistSong.new
 
-  def show
-    @playlist = Playlist.find params[:id]
-    @songs = PlaylistSong.where(playlist_id: @playlist.id).order('created_at DESC')
+			# set song properties and save
+                        song.playlist_id = @playlist.id
+                        song.song_title = params[:new_song][:song_title]
+                        song.artist_name = params[:new_song][:artist_name]
+                        song.save
+                end
 
-    @uid = []
+		# gather all songs for current playlist
+                @songs = @playlist.songs
+        end
 
-    for song in @songs
-      play = Spotify.lucky "#{song.song_title}"
-      song.song_uid = play[:uid] 
-      song.artist_name = play[:artist]
-      song.save
+        def show
+                @playlist = Playlist.find params[:id]
+                @songs = PlaylistSong.where(playlist_id: @playlist.id).order('created_at DESC')
 
-      @uid.push(song.song_uid)
-    end
+                @uid = []
 
-      @list_of_songs = @uid.join(",")
+                for song in @songs
+                        play = Spotify.lucky "#{song.song_title}"
+                        song.song_uid = play[:uid] 
+                        song.artist_name = play[:artist]
+                        song.save
 
-    if params[:notes] != nil
-        @songs.notes = params[:notes]
-    end
+                        @uid.push(song.song_uid)
+                end
 
-  end
-  
+                @list_of_songs = @uid.join(",")
 
-  def destroy
-    @playlist = Playlist.find params[:id]
-    @playlist.destroy
-    redirect_to root_path
-  end
+                if params[:notes] != nil
+                        @songs.notes = params[:notes]
+                end
+        end 
+
+        def destroy
+                @playlist = Playlist.find params[:id]
+                @playlist.destroy
+                redirect_to root_path
+        end
 
 end
--- tapedeck/app/models/playlist_song.rb
+++ tapedeck/app/models/playlist_song.rb
@@ -1,3 +1,26 @@
+gem 'rspotify'
+gem 'awesome_print'
+
 class PlaylistSong < ActiveRecord::Base
-  belongs_to :playlist
+	belongs_to :playlist
+
+	def get
+		RSpotify::Track.find(self.song_uid)
+	end
+	
+	def image
+		get.album.images[0]["url"]
+	end
+
+	def preview
+		get.preview_url
+	end
+
+	def duration
+		get.duration_ms/1000
+	end
+
+	def spotify
+		get.external_urls["spotify"]
+	end
 end
